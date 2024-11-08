d540bc70c418dc72966eea3b91330dbe728bcb95
AntonRomanuyk <anton.romanuyk1@gmail.com>
2024-11-08 07:09:16 +0200

    Add User CRUD

---
+++ quiz_users/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'image_path', 'created_at', 'updated_at']
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'created_at']
+        ordering = ['created_at']
---
+++ quiz_users/signals.py
@@ -0,0 +1,17 @@
+import logging
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import User
+
+logger = logging.getLogger(__name__)
+
+@receiver(post_save, sender=User)
+def log_user_update(sender, instance, created, **kwargs):
+    if created:
+        logger.info(f"User created: {instance.username}")
+    else:
+        logger.info(f"User updated: {instance.username}")
+
+@receiver(post_delete, sender=User)
+def log_user_delete(sender, instance, **kwargs):
+    logger.info(f"User deleted: {instance.username}")
