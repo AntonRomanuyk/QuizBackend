1916019bb535af6fe3f37e024db65682fdf14d6d
AntonRomanuyk <anton.romanuyk1@gmail.com>
2024-11-12 23:08:29 +0200

    Add authorization

--- quiz_users/urls.py
+++ quiz_users/urls.py
@@ -7,4 +7,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
